b63cc6988629d6611586f424fc7d4af1a83badbf
shishir-srivastava <shishir.srivastava@flipkart.com>
2019-09-20 17:20:53 +0530

    Adds alternative solution

--- src/question-2/index.ts
+++ src/question-2/index.ts
@@ -26,3 +26,63 @@ export const solution1 = (list: ReadonlyArray<number>): number[] => {
 
     return computedList;
 };
+
+export const solution2 = (list: ReadonlyArray<number>): number[] => {
+    let numZero: number = 0;
+    let zeroElementIndex: number = -1;
+    const computedList: number[] = [];
+
+    for (let index = 0; index < list.length; index++) {
+        if (list[index] === 0) {
+            numZero++;
+            zeroElementIndex = index;
+        }
+    }
+
+    if (numZero === 1) {
+        // If there is one zero in the list, then only that index in returned array
+        // will have a non-zero result
+        let totalProduct = 1;
+        for (let index = 0; index < list.length; index++) {
+            if (index !== zeroElementIndex) {
+                totalProduct *= list[index];
+            }
+            computedList.push(0);
+        }
+        computedList[zeroElementIndex] = totalProduct;
+    } else if (numZero > 1) {
+        // If there are more than one zero in the list, then all products will be zero
+        list.forEach(() => {
+            computedList.push(0);
+        });
+    } else {
+        // There are no zeros in the list, so computation has to proceed as usual
+        // We create two arrays: left[i] contains product of all numbers to the left of list[i]
+        // not including the i-th element.
+        // right[i] contains product of all numbers to the right of list[i]
+        // not including the i-th element.
+        // The solution at i-th position is product of left[i] and right[i]
+
+        const left: number[] = [];
+        const right: number[] = [];
+
+        // At the first element, there are no elements to the left, so product is 1
+        left[0] = 1;
+        for (let index = 1; index < list.length; index++) {
+            left[index] = left[index - 1] * list[index - 1];
+        }
+
+        // At the last element, there are no elements to the right, so product is 1
+        right[list.length - 1] = 1;
+        for (let index = list.length - 2; index >= 0; index--) {
+            right[index] = right[index + 1] * list[index + 1];
+        }
+
+        // Final solution is calculated by multiplying elements in left and right arrays
+        for (let index = 0; index < list.length; index++) {
+            computedList[index] = left[index] * right[index];
+        }
+    }
+
+    return computedList;
+};
